3bb136d8bca8fa51107ffa321d65ce8b4b8bd49d
TE282179 <julian.flicker@te.com>
2024-12-03 08:12:45 +0100

    Improved WinConsole. Added ShellThumbnail.GetExeIcon()

--- PlayifyUtility.Windows/Features/ShellThumbnail.cs
+++ PlayifyUtility.Windows/Features/ShellThumbnail.cs
@@ -20,7 +20,7 @@
  */
 
 using System.Diagnostics.CodeAnalysis;
-using System.Drawing.Imaging;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using JetBrains.Annotations;
 
@@ -29,38 +29,8 @@ namespace PlayifyUtility.Windows.Features;
 [SuppressMessage("ReSharper","IdentifierTypo")]
 [SuppressMessage("ReSharper","CommentTypo")]
 [PublicAPI]
-public static class ShellThumbnail{
-	[Flags]
-	public enum ThumbnailOptions// IShellItemImageFactory Flags: https://msdn.microsoft.com/en-us/library/windows/desktop/bb761082%28v=vs.85%29.aspx
-	{
-		None=0x00,// Shrink the bitmap as necessary to fit, preserving its aspect ratio. Returns thumbnail if available, else icon.
-		BiggerSizeOk=0x01,// Passed by callers if they want to stretch the returned image themselves. For example, if the caller passes an icon size of 80x80, a 96x96 thumbnail could be returned. This action can be used as a performance optimization if the caller expects that they will need to stretch the image. Note that the Shell implementation of IShellItemImageFactory performs a GDI stretch blit. If the caller wants a higher quality image stretch than provided through that mechanism, they should pass this flag and perform the stretch themselves.
-		InMemoryOnly=0x02,// Return the item only if it is already in memory. Do not access the disk even if the item is cached. Note that this only returns an already-cached icon and can fall back to a per-class icon if an item has a per-instance icon that has not been cached. Retrieving a thumbnail, even if it is cached, always requires the disk to be accessed, so GetImage should not be called from the UI thread without passing SIIGBF_MEMORYONLY.
-		IconOnly=0x04,// Return only the icon, never the thumbnail.
-		ThumbnailOnly=0x08,// Return only the thumbnail, never the icon. Note that not all items have thumbnails, so SIIGBF_THUMBNAILONLY will cause the method to fail in these cases.
-		InCacheOnly=0x10,// Allows access to the disk, but only to retrieve a cached item. This returns a cached thumbnail if it is available. If no cached thumbnail is available, it returns a cached per-instance icon but does not extract a thumbnail or icon.
-		Win8CropToSquare=0x20,// Introduced in Windows 8. If necessary, crop the bitmap to a square.
-		Win8WideThumbnails=0x40,// Introduced in Windows 8. Stretch and crop the bitmap to a 0.7 aspect ratio.
-		Win8IconBackground=0x80,// Introduced in Windows 8. If returning an icon, paint a background using the associated app's registered background color.
-		Win8ScaleUp=0x100,// Introduced in Windows 8. If necessary, stretch the bitmap so that the height and width fit the given size.
-	}
+public static partial class ShellThumbnail{
 
-
-	private const string ShellItem2Guid="7E9FB0D3-919F-4307-AB2E-9B1860310C93";
-
-	[DllImport("shell32.dll",CharSet=CharSet.Unicode,SetLastError=true)]
-	private static extern int SHCreateItemFromParsingName([MarshalAs(UnmanagedType.LPWStr)]string path,
-	                                                      // The following parameter is not used - binding context.
-	                                                      IntPtr pbc,
-	                                                      ref Guid guid,
-	                                                      [MarshalAs(UnmanagedType.Interface)]out IShellItem shellItem);
-
-	[DllImport("gdi32.dll")]
-	[return:MarshalAs(UnmanagedType.Bool)]
-	private static extern bool DeleteObject(IntPtr hObject);
-
-	[DllImport("msvcrt.dll",CallingConvention=CallingConvention.Cdecl)]
-	private static extern IntPtr memcpy(IntPtr dest,IntPtr src,int count);
 
 	public static Bitmap? GetThumbnail(string fileName,out string displayName)=>GetThumbnail(fileName,160,160,ThumbnailOptions.None,out displayName);
 
@@ -92,94 +62,43 @@ public static class ShellThumbnail{
 		return clonedBitmap;
 	}
 
-	private static Bitmap GetBitmapFromHBitmap(IntPtr nativeHBitmap){
-		var bmp=Image.FromHbitmap(nativeHBitmap);
 
-		if(Image.GetPixelFormatSize(bmp.PixelFormat)<32)
-			return bmp;
-
-		return CreateAlphaBitmap(bmp,PixelFormat.Format32bppArgb);
+	public static Icon? GetOwnExeIcon(bool large){
+		try{
+			if(Assembly.GetEntryAssembly()?.Location is{Length: >0} file) return GetExeIcon(file,large);
+		} catch(NotSupportedException){
+		}
+		return null;
 	}
 
-	private static Bitmap CreateAlphaBitmap(Bitmap srcBitmap,PixelFormat targetPixelFormat){
-		var result=new Bitmap(srcBitmap.Width,srcBitmap.Height,targetPixelFormat);
-
-		var bmpBounds=new Rectangle(0,0,srcBitmap.Width,srcBitmap.Height);
-		var srcData=srcBitmap.LockBits(bmpBounds,ImageLockMode.ReadOnly,srcBitmap.PixelFormat);
-		var destData=result.LockBits(bmpBounds,ImageLockMode.ReadOnly,targetPixelFormat);
-
-		var srcDataPtr=srcData.Scan0;
-		var destDataPtr=destData.Scan0;
+	public static Icon? GetExeIcon(string file,bool large){
+		var hDummy=new[]{IntPtr.Zero};
+		var hIconEx=new[]{IntPtr.Zero};
 
 		try{
-			for(var y=0;y<=srcData.Height-1;y++){
-				for(var x=0;x<=srcData.Width-1;x++){
-					//this is really important because one stride may be positive and the other negative
-					var position=srcData.Stride*y+4*x;
-					var position2=destData.Stride*y+4*x;
+			var readIconCount=large?ExtractIconEx(file,0,hIconEx,hDummy,1):ExtractIconEx(file,0,hDummy,hIconEx,1);
 
-					memcpy(destDataPtr+position2,srcDataPtr+position,4);
-				}
-			}
+			if(readIconCount>0&&hIconEx[0]!=IntPtr.Zero){
+				// GET FIRST EXTRACTED ICON
+				var extractedIcon=(Icon)Icon.FromHandle(hIconEx[0]).Clone();
+
+				return extractedIcon;
+			} else// NO ICONS READ
+				return null;
+		} catch(Exception ex){
+			/* EXTRACT ICON ERROR */
+
+			// BUBBLE UP
+			throw new ApplicationException("Could not extract icon",ex);
 		} finally{
-			srcBitmap.UnlockBits(srcData);
-			result.UnlockBits(destData);
+			// RELEASE RESOURCES
+			foreach(var ptr in hIconEx)
+				if(ptr!=IntPtr.Zero)
+					DestroyIcon(ptr);
+
+			foreach(var ptr in hDummy)
+				if(ptr!=IntPtr.Zero)
+					DestroyIcon(ptr);
 		}
-
-		using(srcBitmap)
-			return result;
-	}
-
-	private static IntPtr GetHBitmap(string fileName,int width,int height,ThumbnailOptions options,out string displayName){
-		var shellItem2Guid=new Guid(ShellItem2Guid);
-		var retCode=SHCreateItemFromParsingName(fileName,IntPtr.Zero,ref shellItem2Guid,out var nativeShellItem);
-
-		if(retCode!=0) throw Marshal.GetExceptionForHR(retCode)!;
-
-		nativeShellItem.GetDisplayName(0,out displayName);
-
-		var nativeSize=new NativeSize{
-			Width=width,
-			Height=height,
-		};
-
-		// ReSharper disable once SuspiciousTypeConversion.Global
-		var hr=((IShellItemImageFactory) nativeShellItem).GetImage(nativeSize,options,out var hBitmap);
-
-		Marshal.ReleaseComObject(nativeShellItem);
-
-		if(hr==0) return hBitmap;
-		throw Marshal.GetExceptionForHR(hr)!;
-	}
-
-	[ComImport]
-	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-	[Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
-	internal interface IShellItem{
-		void BindToHandler(IntPtr pbc,
-		                   [MarshalAs(UnmanagedType.LPStruct)]Guid bhid,
-		                   [MarshalAs(UnmanagedType.LPStruct)]Guid riid,
-		                   out IntPtr ppv);
-
-		void GetParent(out IShellItem ppsi);
-		void GetDisplayName(uint sigdnName,[MarshalAs(UnmanagedType.LPWStr)]out string ppszName);
-		void GetAttributes(uint sfgaoMask,out uint psfgaoAttribs);
-		void Compare(IShellItem psi,uint hint,out int piOrder);
-	}
-
-	[ComImport]
-	[Guid("bcc18b79-ba16-442f-80c4-8a59c30c463b")]
-	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-	private interface IShellItemImageFactory{
-		[PreserveSig]
-		int GetImage([In,MarshalAs(UnmanagedType.Struct)]NativeSize size,
-		             [In]ThumbnailOptions flags,
-		             [Out]out IntPtr phbm);
-	}
-
-	[StructLayout(LayoutKind.Sequential)]
-	private struct NativeSize{
-		public int Width;
-		public int Height;
 	}
 }
--- PlayifyUtility.Windows/Win/WinConsole.cs
+++ PlayifyUtility.Windows/Win/WinConsole.cs
@@ -2,9 +2,7 @@ using System.Reflection;
 using System.Text;
 using JetBrains.Annotations;
 using Microsoft.Win32.SafeHandles;
-using PlayifyUtility.Windows.Features.Hooks;
-using PlayifyUtility.Windows.Features.Interact;
-using PlayifyUtility.Windows.Win.Native;
+using PlayifyUtility.Windows.Features;
 
 namespace PlayifyUtility.Windows.Win;
 
@@ -12,88 +10,90 @@ namespace PlayifyUtility.Windows.Win;
 public static partial class WinConsole{
 	public static WinWindow ConsoleWindow=>new(GetConsoleWindow());
 
+	public static bool RunningInRider=>ConsoleWindow.Class=="PseudoConsoleWindow";
+
 	public static bool Visible{
-		get=>ConsoleWindow.IsVisible;
+		get=>ConsoleWindow.NonZero(out var console)&&console.IsVisible;
 		set{
 			var window=ConsoleWindow;
+			if(window==WinWindow.Zero) return;
+			if(RunningInRider) return;
 			window.IsVisible=value;
 			if(value) window.SetForeground();
 		}
 	}
 
-	public static bool EnableAnsi(){
-		var iStdOut=GetStdHandle(-11);//STD_OUTPUT_HANDLE
-		if(!GetConsoleMode(iStdOut,out var outConsoleMode)) return false;
-		outConsoleMode|=0x0004|0x0008;//ENABLE_VIRTUAL_TERMINAL_PROCESSING|DISABLE_NEWLINE_AUTO_RETURN
-		return SetConsoleMode(iStdOut,outConsoleMode);
+	public static bool EnableAnsi()=>EnableAnsi(GetStdHandle(-11))&EnableAnsi(GetStdHandle(-12));//STD_OUTPUT_HANDLE,STD_ERROR_HANDLE
+
+	private static bool EnableAnsi(IntPtr stream)=>
+		GetConsoleMode(stream,out var mode)&&
+		SetConsoleMode(stream,mode|1|4|8);//ENABLE_PROCESSED_OUTPUT|ENABLE_VIRTUAL_TERMINAL_PROCESSING|DISABLE_NEWLINE_AUTO_RETURN
+
+
+	public static WinWindow CreateConsole(){
+		if(!AllocConsole()) return ConsoleWindow;//Already has one allocated
+		if(!ConsoleWindow.NonZero(out var console)) throw new SystemException("Error creating Console");//Failed to get newly allocated one
+
+		var conIn=CreateFile("CONIN$",GenericRead,FileShareRead,0,OpenExisting,0,0);
+		Console.SetIn(new StreamReader(
+			new FileStream(
+				new SafeFileHandle(
+					conIn,
+					true),
+				FileAccess.Read),
+			Encoding.Default));
+
+		var conOut=CreateFile("CONOUT$",GenericWrite,FileShareWrite,0,OpenExisting,0,0);
+		Console.SetOut(new StreamWriter(
+			new FileStream(
+				new SafeFileHandle(
+					conOut,
+					true),
+				FileAccess.Write),
+			Encoding.Default){
+			AutoFlush=true,
+		});
+		Console.SetError(Console.Out);
+
+
+		//For net48, Console has hidden fields, that need to be overridden, to enable Console.ReadKey()
+		typeof(Console).GetField("_consoleInputHandle",BindingFlags.NonPublic|BindingFlags.Static)?.SetValue(null,conIn);
+		typeof(Console).GetField("_consoleOutputHandle",BindingFlags.NonPublic|BindingFlags.Static)?.SetValue(null,conOut);
+
+		//For net6, Console accesses the StdHandle every time, so it needs to be set correctly
+		SetStdHandle(-10,conIn);
+		SetStdHandle(-11,conOut);
+		SetStdHandle(-12,conOut);
+		return console;
 	}
 
 
-	/**Can be hidden using Win+Alt+M and shown using Win+Alt+N*/
-	public static void CreateConsole()=>CreateHideAbleConsole(false);
+	public static WinWindow CreateHiddenConsole(){
+		var console=CreateConsole();
+		if(!RunningInRider) console.IsVisible=false;
+		return console;
+	}
 
-	/**Can be hidden using Win+Alt+M and shown using Win+Alt+N*/
-	public static void CreateHiddenConsole()=>CreateHideAbleConsole(true);
-
-	private static void CreateHideAbleConsole(bool hidden){
-		if(AllocConsole()&&ConsoleWindow.NonZero(out var console)){
-			if(hidden) console.Hidden=true;
-
-			var conIn=CreateFile("CONIN$",GenericRead,FileShareRead,0,OpenExisting,0,0);
-			Console.SetIn(new StreamReader(
-				new FileStream(
-					new SafeFileHandle(
-						conIn,
-						true),
-					FileAccess.Read),
-				Encoding.Default));
-
-			var conOut=CreateFile("CONOUT$",GenericWrite,FileShareWrite,0,OpenExisting,0,0);
-			Console.SetOut(new StreamWriter(
-				new FileStream(
-					new SafeFileHandle(
-						conOut,
-						true),
-					FileAccess.Write),
-				Encoding.Default){
-				AutoFlush=true,
-			});
-			Console.SetError(Console.Out);
+	public static NotifyIcon CreateHiddenConsole(string notifyIconName,bool consoleTitle=true){
+		if(consoleTitle) Console.Title=notifyIconName;
 
 
-			//For net48, Console has hidden fields, that need to be overridden, to enable Console.ReadKey()
-			typeof(Console).GetField("_consoleInputHandle",BindingFlags.NonPublic|BindingFlags.Static)?.SetValue(null,conIn);
-			typeof(Console).GetField("_consoleOutputHandle",BindingFlags.NonPublic|BindingFlags.Static)?.SetValue(null,conOut);
-
-			//For net6, Console accesses the StdHandle every time, so it needs to be set correctly
-			SetStdHandle(-10,conIn);
-			SetStdHandle(-11,conOut);
-			SetStdHandle(-12,conOut);
-
-			if(!hidden) return;
-		} else if(!hidden) return;
-		else (console=ConsoleWindow).HidePush();
-
-		GlobalKeyboardHook.KeyDown+=e=>{
-			if(e.Handled) return;
-
-			switch(e.Key){
-				case Keys.M when Modifiers.Win&&Modifiers.Alt:{
-					if(console.IsVisible&&WinWindow.Foreground==console){
-						e.Handled=true;
-						console.ShowWindowCommand=ShowWindowCommands.Hide;
-					}
-					break;
-				}
-				case Keys.N when Modifiers.Win&&Modifiers.Alt:{
-					if(!console.IsVisible){
-						e.Handled=true;
-						console.ShowWindowCommand=ShowWindowCommands.Show;
-						console.SetForeground();
-					}
-					break;
-				}
-			}
+		var notifyIcon=new NotifyIcon{
+			Icon=ShellThumbnail.GetOwnExeIcon(false)??SystemIcons.Application,
+			Text=$"{notifyIconName} (Hidden)",
+			Visible=true,
 		};
+
+		if(RunningInRider) notifyIcon.Text=notifyIconName;
+		else{
+			var console=CreateHiddenConsole();
+			notifyIcon.Click+=(_,_)=>{
+				var visible=console.IsVisible^=true;
+				notifyIcon.Text=$"{notifyIconName} ({(visible?"Visible":"Hidden")})";
+				if(visible) console.SetForeground();
+			};
+		}
+
+		return notifyIcon;
 	}
 }
